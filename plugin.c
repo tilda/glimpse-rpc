91c56d4fa85038b199a3678c5396b6ab43aee621
S Stewart <tilda@users.noreply.github.com>
2020-11-10 14:55:38 -0600

    add query method for plugin

--- plugin.c
+++ plugin.c
@@ -16,7 +16,7 @@ GimpPlugInInfo PLUG_IN_INFO {
     run
 };
 
-MAIN(); // cringe
+MAIN() // cringe
 
 static void init(void) {
 
@@ -27,7 +27,29 @@ static void quit(void) {
 };
 
 static void query(void) {
+    time_t timev;
 
+
+    static GimpParamDef args[] {
+        {
+            GIMP_PDB_INT32,
+            "run-mode",
+            "Run mode"
+        }
+    };
+
+    gimp_install_procedure {
+        "plug-in-glimpse-rpc",
+        "Glimpse RPC for Discord",
+        "Displays your GIMP/Glimpse activity in your Discord profile.",
+        "S Stewart (tilda)",
+        "tilda, under the Apache License 2.0",
+        "2020",
+        "",
+        GIMP_EXTENSION
+    };
+
+    gimp_extension_ack();
 };
 
 static void run(const gchar *name, gint nparams, const GimpParam *param, gint *nreturn_vals, GimpParam **return_vals) {
