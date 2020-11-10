e402314ec18f4d27cd89411aaf339c4ddd2e69ed
S Stewart <tilda@users.noreply.github.com>
2020-11-09 23:02:33 -0600

    initial code commit

---
+++ plugin.c
@@ -0,0 +1,36 @@
+#include <libgimp/gimp.h>
+
+static void init  (void);
+static void quit  (void);
+static void query (void);
+static void run   (const gchar      *name,
+                   gint              nparams,
+                   const GimpParam  *param,
+                   gint             *nreturn_vals,
+                   GimpParam       **return_vals);
+
+GimpPlugInInfo PLUG_IN_INFO {
+    init,
+    quit,
+    query,
+    run
+};
+
+MAIN() // cringe
+
+static void init(void) {
+
+};
+
+static void quit(void) {
+
+};
+
+static void query(void) {
+
+};
+
+static void run(const gchar *name, gint nparams, const GimpParam *param, gint *nreturn_vals, GimpParam **return_vals) {
+
+};
+
